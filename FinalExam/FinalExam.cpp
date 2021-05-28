88cedc9514cb9656e50f64db905c04fdc82e56cd
Dmytro Venchak <venchak.d@live.unic.ac.cy>
2021-05-28 09:42:48 +0300

    Add project files.

---
+++ FinalExam/FinalExam.cpp
@@ -0,0 +1,17 @@
+/** \file FinalExam.cpp
+ *  \brief     A final exam work of Dmytro Venchak
+ *  \details   This programm will have a functionality to manipulate an array of integers 
+ *              through a users input
+ *  \author    Dmytro Venchak
+ *  \version   0.1
+ *  \date      28/05/2021
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+}
+
